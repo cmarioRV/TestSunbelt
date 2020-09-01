1f4130c98f9fc37f59a4431cc93faac31438bac3
marioRV <mariomad18@gmail.com>
2020-09-01 16:55:35 -0500

    Commit inicial

---
+++ TestSunbelt/TestSunbelt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSunbelt
+//
+//  Created by Mario Rúa on 1/09/20.
+//  Copyright © 2020 Mario Rúa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
